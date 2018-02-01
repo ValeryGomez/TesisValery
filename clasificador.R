a0da32ff9139808666e9a5bee05751cbca3e8813
ValeryGomez <30806926+ValeryGomez@users.noreply.github.com>
2018-02-01 15:45:40 -0600

    Add files via upload

--- clasificador.R
+++ clasificador.R
@@ -1,7 +1,16 @@
+library(ggplot2)
 data = read.csv("inv_limpio.csv", header = TRUE, sep=" ")
 n = length(data[1,])
-limite = 6 
+activ <- data.frame()
+
+##Ciclo para variar los l??mites 1-Activos 2-clientes que abandonaron
+for(a in 1:9){
+limite = a 
 activos = numeric()
+aa<- numeric()
+xt = 1:(n-1)
+xl = c(1:12, 1:12, 1:12, 1:4)
+
 for (i in 1:length(data$client)) {
   historico = unlist(data[i,])
   contador = 0
@@ -25,22 +34,49 @@ for (i in 1:length(data$client)) {
   activos = c(activos, contador >= limite)
 }
 data$activo = activos
+aa <-c(limite,sum(data$activo),i-sum(data$activo))
+activ <- rbind(activ,aa)
 
-##Graficos de activos##
-xt = 1:(n-1)
-xl = c(1:12, 1:12, 1:12, 1:4)
-for (i in 1:k) {
-  main = paste("Client", data[i,]$client, sep=" ")
-  filename = paste(data[i,]$client, ".png", sep="")
-  png(filename, width=1200, height=1000)
-  if(data[i,]$activo==1){
-    plot(xt, data[i,2:n], type="s", xaxt="n", lwd=3,
-         xlab="From 2014 to present", ylab="Client's inventory", col="dark red")
-    axis(side=1, at=1:(n-1), labels=xl)
-  }else{
-    plot(xt, data[i,2:n], type="s", xaxt="n", lwd=3,
-         xlab="From 2014 to present", ylab="Client's inventory", col="dark green")
-    axis(side=1, at=1:(n-1), labels=xl)
-  }
-  graphics.off()
+###Ejemplo visual###
+main = paste("Clientes en ",limite," meses (5 Clientes)", sep=" ")
+filename = paste(limite,"mesesEjemplo.png", sep="")
+png(filename, width=1200, height=1000)
+if(data[1,]$activo==1){color="dark red"}else{color="dark green" }
+plot(xt, data[1,2:n], type="s", xaxt="n", lwd=3,
+     xlab="From 2014 to present", ylab="Client's inventory", col=color)
+for (i in 2:5){
+  color = if(data[i,]$activo==1){color="dark red"}else{color="dark green" }
+  points(xt, data[i,2:n], type="s", lwd=3, col=color)
 }
+axis(side=1, at=1:(n-1), labels=xl)
+graphics.off()
+###FinEjemplo###
+}
+colnames(activ)<- c("meses","activos","noactivos")
+library(reshape2)
+library(plyr)
+library(geom_bar)
+xlong <- ddply(melt(activ, id.vars = 'meses'), .(meses), mutate, prop = value / sum(value))
+png("mesesPorciento.png", width=1200, height=1000)
+ggplot(xlong, aes(x= meses, y = prop, fill = variable)) + geom_bar(stat = 'identity') + xlab("Meses sin actividad") + ylab("Porcentaje de Activos/No Activos") + theme(axis.text=element_text(size=16),
+        axis.title=element_text(size=18,face="bold"),legend.title = element_text(size=18,face="bold"),legend.text = element_text(size=20))
+graphics.off()
+#####Gr??fico con todas las series
+#xt = 1:(n-1)
+#xl = c(1:12, 1:12, 1:12, 1:4)
+#for (i in 1:k) {
+#  main = paste("Client", data[i,]$client, sep=" ")
+#  filename = paste(data[i,]$client, ".png", sep="")
+#  png(filename, width=1200, height=1000)
+#  if(data[i,]$activo==1){
+#    plot(xt, data[i,2:n], type="s", xaxt="n", lwd=3,
+#         xlab="From 2014 to present", ylab="Client's inventory", col="dark red")
+#    axis(side=1, at=1:(n-1), labels=xl)
+#  }else{
+#    plot(xt, data[i,2:n], type="s", xaxt="n", lwd=3,
+#         xlab="From 2014 to present", ylab="Client's inventory", col="dark green")
+#    axis(side=1, at=1:(n-1), labels=xl)
+#  }
+#  graphics.off()
+#}
+##### Final de imagenes, permanecen en comentario para no generarlas
