493272979c42cee3ff342bbd5f611d4f23ad0db0
ValeryGomez <30806926+ValeryGomez@users.noreply.github.com>
2018-02-08 16:54:40 -0600

    Add files via upload

--- clasificador.R
+++ clasificador.R
@@ -4,8 +4,8 @@ n = length(data[1,])
 activ <- data.frame()
 
 ##Ciclo para variar los l??mites 1-Activos 2-clientes que abandonaron
-for(a in 1:9){
-limite = a 
+#for(a in 1:9){
+limite = 6 
 activos = numeric()
 aa<- numeric()
 xt = 1:(n-1)
@@ -34,47 +34,49 @@ for (i in 1:length(data$client)) {
   activos = c(activos, contador >= limite)
 }
 data$activo = activos
-aa <-c(limite,sum(data$activo),i-sum(data$activo))
-activ <- rbind(activ,aa)
+#aa <-c(limite,sum(data$activo),i-sum(data$activo))
+#activ <- rbind(activ,aa)
 
 ###Ejemplo visual###
-main = paste("Clientes en ",limite," meses (5 Clientes)", sep=" ")
-filename = paste(limite,"mesesEjemplo.png", sep="")
-png(filename, width=1200, height=1000)
-if(data[1,]$activo==1){color="dark red"}else{color="dark green" }
-plot(xt, data[1,2:n], type="s", xaxt="n", lwd=3,
-     xlab="From 2014 to present", ylab="Client's inventory", col=color)
-for (i in 2:5){
-  color = if(data[i,]$activo==1){color="dark red"}else{color="dark green" }
-  points(xt, data[i,2:n], type="s", lwd=3, col=color)
-}
-axis(side=1, at=1:(n-1), labels=xl)
-graphics.off()
+#main = paste("Clientes en ",limite," meses (5 Clientes)", sep=" ")
+#filename = paste(limite,"mesesEjemplo.png", sep="")
+#png(filename, width=1200, height=1000)
+#if(data[1,]$activo==1){color="dark red"}else{color="dark green" }
+#plot(xt, data[1,2:n], type="s", xaxt="n", lwd=3,
+#     xlab="From 2014 to present", ylab="Client's inventory", col=color)
+#for (i in 2:5){
+#  color = if(data[i,]$activo==1){color="dark red"}else{color="dark green" }
+#  points(xt, data[i,2:n], type="s", lwd=3, col=color)
+#}
+#axis(side=1, at=1:(n-1), labels=xl)
+#graphics.off()
 ###FinEjemplo###
-}
-colnames(activ)<- c("meses","activos","noactivos")
-library(reshape2)
-library(plyr)
-library(geom_bar)
-xlong <- ddply(melt(activ, id.vars = 'meses'), .(meses), mutate, prop = value / sum(value))
-png("mesesPorciento.png", width=1200, height=1000)
-ggplot(xlong, aes(x= meses, y = prop, fill = variable)) + geom_bar(stat = 'identity') + xlab("Meses sin actividad") + ylab("Porcentaje de Activos/No Activos") + theme(axis.text=element_text(size=16),
-        axis.title=element_text(size=18,face="bold"),legend.title = element_text(size=18,face="bold"),legend.text = element_text(size=20))
-graphics.off()
+#}
+#colnames(activ)<- c("meses","activos","noactivos")
+#library(reshape2)
+#library(plyr)
+#library(geom_bar)
+#xlong <- ddply(melt(activ, id.vars = 'meses'), .(meses), mutate, prop = value / sum(value))
+#png("mesesPorciento.png", width=1200, height=1000)
+#ggplot(xlong, aes(x= meses, y = prop, fill = variable)) + geom_bar(stat = 'identity') + xlab("N??mero de meses sin actividad") + ylab("Porcentaje de Activos/No Activos") + theme(axis.text=element_text(size=16),
+#        axis.title=element_text(size=18,face="bold"),legend.title = element_text(size=18,face="bold"),legend.text = element_text(size=20))+ labs(fill = "Clientes:")
+#graphics.off()
+
+
 #####Gr??fico con todas las series
 #xt = 1:(n-1)
 #xl = c(1:12, 1:12, 1:12, 1:4)
-#for (i in 1:k) {
+#for (i in 1:length(data$client)) {
 #  main = paste("Client", data[i,]$client, sep=" ")
 #  filename = paste(data[i,]$client, ".png", sep="")
 #  png(filename, width=1200, height=1000)
 #  if(data[i,]$activo==1){
 #    plot(xt, data[i,2:n], type="s", xaxt="n", lwd=3,
-#         xlab="From 2014 to present", ylab="Client's inventory", col="dark red")
+#         xlab="From 2014 to present", ylab="Client's inventory", col="red")
 #    axis(side=1, at=1:(n-1), labels=xl)
 #  }else{
 #    plot(xt, data[i,2:n], type="s", xaxt="n", lwd=3,
-#         xlab="From 2014 to present", ylab="Client's inventory", col="dark green")
+#         xlab="From 2014 to present", ylab="Client's inventory", col="green")
 #    axis(side=1, at=1:(n-1), labels=xl)
 #  }
 #  graphics.off()
--- tendencias.R
+++ tendencias.R
@@ -1,15 +1,163 @@
 data = read.csv("inv_limpio.csv", header = TRUE, sep=" ")
 n = length(data[1,])
 
+###
+library(ggplot2)
+activ <- data.frame()
+##Ciclo para variar los l??mites 1-Activos 2-clientes que abandonaron
+limite = 6 
+activos = numeric()
+aa<- numeric()
+xt = 1:(n-1)
+xl = c(1:12, 1:12, 1:12, 1:4)
+
+for (i in 1:length(data$client)) {
+  historico = unlist(data[i,])
+  contador = 0
+  pos = n
+  while (contador < limite & is.na(historico[pos])) {
+    contador = contador + 1
+    pos = pos - 1
+  }
+  if (contador < limite & !is.na(historico[pos])) {
+    cola = historico[pos]
+    while (pos > 0 && !is.na(historico[pos - 1])) {
+      pos = pos - 1
+      # cat(historico[pos], cola, "\n")
+      if (historico[pos] == cola) {
+        contador = contador + 1
+      } else {
+        break
+      }
+    }
+  }
+  activos = c(activos, contador >= limite)
+}
+data$activo = activos
+####
+Mtodas <- data()
+##
+medidas = function(m,y,k){
+  T = length(y)
+  yest = fitted(m)
+  sse = sum((yest-y)^2)
+  ssr = sum((y-mean(y))^2)
+  mse = sse/(T-k)
+  R2 = 1 - sse/ssr
+  Ra2 = 1 - (T-1)*(1-R2)/(T-k)
+  aic = log((T-k)*exp(2*k/T)*mse/T)
+  bic = log(T^(k/T)*(T-k)*mse/T)
+  M = c(Ra2, mse, aic, bic)
+  names(M) = c("R2-ajus","MSE","logAIC","logBIC")
+  return(M)}
+##
 for(i in 1:length(data$client)){
   i<-1
-  cl <- data[i,6:n]
-  y = na.omit(cl)
-  y <- ts(y,frequency=12,start=c(2014,05))
+  cl <- t(data[i,2:n])
+  y <- na.omit(cl)
+  y <- ts(y,frequency=12,start=c(2014,01))
   ##
-  fechas = seq(as.Date("2014/1/1"), length.out = length(y), by = "months")
-  ts.plot(y,main="Inventario de Cliente")
-  np = length(y)
-  ejex.mes = seq(fechas[1],fechas[np], "months")
-  ejex.anno = seq(fechas[1],fechas[np],"years")
+#  fechas = seq(as.Date("2014/1/1"), length.out = length(y), by = "months")
+#  ts.plot(y,main="Inventario de Cliente")
+#  np = length(y)
+#  ejex.mes = seq(fechas[1],fechas[np], "months")
+#  ejex.anno = seq(fechas[1],fechas[np],"years")
+#  if(data[i,]$activo==1){color="red"}else{color="green" }
+#  filename = paste(data[i,1],"movimiento.png", sep="")
+#  png(filename, width=1200, height=1000)
+#  plot(fechas,y, xaxt="n", panel.first = grid(),type='l',
+#       ylab='Movimiento de Guias',xlab = 'Meses', lwd = 2, main =paste("Cliente ",data[i,1]), col=color )
+#  axis.Date(1, at=ejex.mes, format="%m/%y")
+#  axis.Date(1, at=ejex.anno, labels = FALSE, tcl = -0.2)
+#  graphics.off()
+  ##An??lisis##
+  
+  #Inicio de an??lisis#
+  T = length(y)
+  yi = y[1:(T-12)]
+  yf = y[(T-12+1):T]
+  ##Cuatro modelos ##
+  t = seq(1:(T-12))
+  t2 = t^2
+  t3 = t^3
+  lyi = log(yi)
+  ##
+  mod.lin = lm(yi~t)
+  mod.cuad = lm(yi~t+t2)
+  mod.cub = lm(yi~t+t2+t3)
+  mod.llin = lm(lyi~t)
+  ###
+  summary(mod.lin)
+  summary(mod.cuad)
+  summary(mod.cub)
+  #1
+  mod.llin = lm(lyi~t)
+  #2
+  b0.est = mod.llin$coefficient[1]
+  b1.est = mod.llin$coefficient[2]
+  #3
+  Ds = data.frame(yi,t)
+  #4
+  mod.exp = nls(yi~exp(beta0+beta1*t), data=Ds, start=list(beta0=b0.est, beta1=b1.est))
+  #5
+  summary(mod.exp)
+  ##Todos##
+  M.lin = medidas(mod.lin,yi,2)
+  M.cuad = medidas(mod.cuad,yi,3)
+  M.cub = medidas(mod.cub,yi,4)
+  M.exp = medidas(mod.exp,yi,2)
+  M = cbind(M.lin,M.cuad,M.cub,M.exp)
+  Mtodas <- rbind(Mtodas,M)
 }
+
+#Inicio de an??lisis#
+T = length(y)
+yi = y[1:(T-12)]
+yf = y[(T-12+1):T]
+##Cuatro modelos ##
+t = seq(1:(T-12))
+t2 = t^2
+t3 = t^3
+lyi = log(yi)
+##
+mod.lin = lm(yi~t)
+mod.cuad = lm(yi~t+t2)
+mod.cub = lm(yi~t+t2+t3)
+mod.llin = lm(lyi~t)
+###
+summary(mod.lin)
+summary(mod.cuad)
+summary(mod.cub)
+#1
+mod.llin = lm(lyi~t)
+#2
+b0.est = mod.llin$coefficient[1]
+b1.est = mod.llin$coefficient[2]
+#3
+Ds = data.frame(yi,t)
+#4
+mod.exp = nls(yi~exp(beta0+beta1*t), data=Ds, start=list(beta0=b0.est, beta1=b1.est))
+#5
+summary(mod.exp)
+##Todos##
+medidas = function(m,y,k){
+T = length(y)
+yest = fitted(m)
+sse = sum((yest-y)^2)
+ssr = sum((y-mean(y))^2)
+mse = sse/(T-k)
+R2 = 1 - sse/ssr
+Ra2 = 1 - (T-1)*(1-R2)/(T-k)
+aic = log((T-k)*exp(2*k/T)*mse/T)
+bic = log(T^(k/T)*(T-k)*mse/T)
+M = c(Ra2, mse, aic, bic)
+names(M) = c("R2-ajus","MSE","logAIC","logBIC")
+return(M)}
+
+M.lin = medidas(mod.lin,yi,2)
+M.cuad = medidas(mod.cuad,yi,3)
+M.cub = medidas(mod.cub,yi,4)
+M.exp = medidas(mod.exp,yi,2)
+M = cbind(M.lin,M.cuad,M.cub,M.exp)
+
+r = mod.cub$residuals
